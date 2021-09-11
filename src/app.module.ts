99ac44584615091baa9834ad13c90c13e9788262
dongyonglee <pfldy2850@gmail.com>
2021-09-11 17:31:36 +0900

    Update generated schema.gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { DishesModule } from './dishes/dishes.module';
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gpl'),
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
       debug: true,
       playground: true,
